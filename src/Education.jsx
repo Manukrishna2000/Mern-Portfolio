8317e250890144ece8fe67283ac6c5a43b56963e
Manukrishna2000 <manukrishnaap@gmail.com>
2024-07-04 15:48:28 +0530

    changed new

--- src/Education.jsx
+++ src/Education.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Education = () => {
   return (
-    <div className="background2">
+    <div className="">
       <div className="bg-black/65 py-16">
         <div className="text-center text-white text-4xl font-semibold mb-12">
           Education
@@ -11,6 +11,9 @@ const Education = () => {
           <div className="flex-1 flex flex-col items-center text-center md:text-left px-6">
             <div className="text-2xl md:text-3xl text-white font-medium mb-2">
               Master Of Computer Applications
+            </div>
+            <div className="text-xl md:text-l text-white font-medium mb-2">
+              2021-2023
             </div>
             <div className="text-lg md:text-xl text-white/70">
               AWH Engineering College - KTU University
@@ -23,6 +26,9 @@ const Education = () => {
           <div className="flex-1 flex flex-col items-center text-center md:text-left px-6">
             <div className="text-2xl md:text-3xl text-white font-medium mb-2">
               Bachelor Of Computer Applications
+            </div>
+            <div className="text-xl md:text-l text-white font-medium mb-2">
+              2018-2021
             </div>
             <div className="text-lg md:text-xl text-white/70">
               UNIVERSITY OF CALICUT
